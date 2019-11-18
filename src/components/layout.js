8f8f54752a124957d82fe266824361d0710c7254
raja <rbellebon@gmail.com>
2019-11-18 16:30:11 +1100

    Update content

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,11 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: 'description',
+              content: 'Down the Code Blog and Pictures.',
+            },
+            { name: 'keywords', content: 'Coding, Trekking, Pictures.' },
           ]}
         >
           <html lang="en" />
